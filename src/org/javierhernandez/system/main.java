ce2317503b3a0aa32ab1ad5cca4c8553eef0f2dc
Javier <javiherdeza@gmail.com>
2024-08-27 09:02:50 -0600

    chore: añadir nuevos directorios para la estructura del proyecto

---
+++ src/org/javierhernandez/system/main.java
@@ -0,0 +1,17 @@
+
+package org.javierhernandez.system;
+
+/**
+ *
+ * @author javih
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
